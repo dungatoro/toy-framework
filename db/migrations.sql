10c113b2f5ce739d8e1448e9f63758048007f6fb
dungatoro <noahjbarker@icloud.com>
2024-02-21 16:20:50 +0000

    fix user_id

--- db/migrations.sql
+++ db/migrations.sql
@@ -4,7 +4,7 @@ Always make use of `if not exists` to avoid overwrites.
 */
 -- Create the users table
 create table if not exists users (
-    user_id       integer primary key,
+    id            integer primary key,
     email         text not null unique,
     username      text not null, 
     password_hash text not null
